f9fc40015f3107f86b81f33fa520848b8574219c
Kevin W Monroe <kevin.monroe@canonical.com>
2021-07-28 10:15:13 -0500

    refactor to be more like k-s-metrics charm; manage_workloads pattern is better

--- src/charm.py
+++ src/charm.py
@@ -16,9 +16,9 @@ import logging
 from charms.nginx_ingress_integrator.v0.ingress import IngressRequires
 
 from ops.charm import CharmBase
-from ops.framework import StoredState
 from ops.main import main
-from ops.model import ActiveStatus, BlockedStatus
+from ops.model import ActiveStatus, BlockedStatus, WaitingStatus
+from ops.pebble import Layer, ConnectionError
 
 logger = logging.getLogger(__name__)
 
@@ -26,73 +26,75 @@ logger = logging.getLogger(__name__)
 class KubeflowVolumesOperatorCharm(CharmBase):
     """Charm the service."""
 
-    _stored = StoredState()
-
     def __init__(self, *args):
         super().__init__(*args)
         self.framework.observe(self.on.kubeflow_volumes_pebble_ready,
-                               self._on_kubeflow_volumes_pebble_ready)
-        self.framework.observe(self.on.config_changed, self._on_config_changed)
+                               self._manage_workload)
+        self.framework.observe(self.on.config_changed, self._manage_workload)
+        self.framework.observe(self.on.upgrade_charm, self._manage_workload)
 
         self.ingress = IngressRequires(
             self,
             {
-                "service-hostname": self._external_hostname,
+                "service-hostname": self.external_hostname,
                 "service-name": self.app.name,
                 "service-port": 8080,
             },
         )
 
+    def _manage_workload(self, event):
+        """Manage the workload using the Pebble API."""
+        if not self._validate_config():
+            return
+
+        self.ingress.update_config({"service-hostname": self.external_hostname})
+
+        try:
+            # Get a reference the container attribute on the PebbleReadyEvent
+            container = event.workload
+            # Add intial Pebble config layer using the Pebble API
+            container.add_layer("kubeflow-volumes", self.layer, combine=True)
+            # Autostart any services that were defined with startup: enabled
+            container.autostart()
+            self.unit.status = ActiveStatus()
+        except ConnectionError:
+            self.unit.status = WaitingStatus("Waiting for Pebble")
+
+    def _validate_config(self):
+        """Check that charm config settings are valid.
+
+        If the charm config is not valid, will set the unit status to BlockedStatus
+        and return False.
+        """
+        if not self.config["port"]:
+            self.unit.status = BlockedStatus("Missing 'port' configuration")
+            return False
+        return True
+
     @property
-    def _external_hostname(self):
-        """
-        Check if hostname has been configured. If not, generate one.
-        """
+    def external_hostname(self):
+        """Check if hostname has been configured. If not, generate one."""
         return self.config["external-hostname"] or "{}.juju".format(self.app.name)
 
-    def _on_kubeflow_volumes_pebble_ready(self, event):
-        """Define and start a workload using the Pebble API.
-
-        Learn more about Pebble layers at https://github.com/canonical/pebble
-        """
-        if not self.model.config.get("port"):
-            self.unit.status = BlockedStatus("Missing 'port' configuration")
-            return
-
-        # Get a reference the container attribute on the PebbleReadyEvent
-        container = event.workload
-        # Define an initial Pebble layer configuration
-        pebble_layer = {
-            "summary": "kubeflow-volumes layer",
-            "description": "pebble config layer for kubeflow-volumes",
-            "services": {
-                "kubeflow-volumes": {
-                    "override": "replace",
-                    "summary": "kubeflow-volumes",
-                    "command": "gunicorn -w 3 --bind 0.0.0.0:{} "
-                               "--access-logfile - entrypoint:app".format(
-                                   self.model.config["port"]),
-                    "startup": "enabled",
-                    "environment": {},
-                }
-            },
-        }
-        # Add intial Pebble config layer using the Pebble API
-        container.add_layer("kubeflow-volumes", pebble_layer, combine=True)
-        # Autostart any services that were defined with startup: enabled
-        container.autostart()
-        self.unit.status = ActiveStatus()
-
-    def _on_config_changed(self, _):
-        """
-        Update the layer with new config.
-        """
-        if not self.model.config.get("port"):
-            self.unit.status = BlockedStatus("Missing 'port' configuration")
-            return
-
-        self.ingress.update_config(
-            {"service-hostname": self.config["external_hostname"]})
+    @property
+    def layer(self):
+        """Pebble layer for workload."""
+        return Layer(
+            {
+                "summary": "kubeflow-volumes layer",
+                "description": "pebble config layer for kubeflow-volumes",
+                "services": {
+                    "kubeflow-volumes": {
+                        "override": "replace",
+                        "summary": "kubeflow-volumes",
+                        "command": "gunicorn -w 3 --bind 0.0.0.0:{} "
+                                   "--access-logfile - entrypoint:app".format(
+                                       self.config["port"]),
+                        "startup": "enabled",
+                    }
+                },
+            }
+        )
 
 
 if __name__ == "__main__":
