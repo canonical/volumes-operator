c1b58c96efefe1382d5f55eacd3d2949689bed08
Kevin W Monroe <kevin.monroe@canonical.com>
2021-07-28 10:15:28 -0500

    add config/ingress unit tests

--- tests/test_charm.py
+++ tests/test_charm.py
@@ -5,7 +5,7 @@
 import unittest
 
 from charm import KubeflowVolumesOperatorCharm
-from ops.model import ActiveStatus
+from ops.model import ActiveStatus, BlockedStatus
 from ops.testing import Harness
 
 
@@ -15,12 +15,15 @@ class TestCharm(unittest.TestCase):
         self.addCleanup(self.harness.cleanup)
         self.harness.begin()
 
-    def test_config_changed(self):
-        self.assertEqual(list(self.harness.charm._stored.things), [])
+    def test_valid_config(self):
         self.harness.update_config({"port": 8888})
-        self.assertEqual(list(self.harness.charm._stored.things), [8888])
+        assert isinstance(self.harness.charm.unit.status, ActiveStatus)
 
-    def test_kubeflow_volumes_pebble_ready(self):
+    def test_invalid_config(self):
+        self.harness.update_config({"port": ""})
+        assert isinstance(self.harness.charm.unit.status, BlockedStatus)
+
+    def test_manage_workload(self):
         # Check the initial Pebble plan is empty
         initial_plan = self.harness.get_container_pebble_plan("kubeflow-volumes")
         self.assertEqual(initial_plan.to_yaml(), "{}\n")
@@ -33,13 +36,12 @@ class TestCharm(unittest.TestCase):
                     "command": "gunicorn -w 3 --bind 0.0.0.0:5000 "
                                "--access-logfile - entrypoint:app",
                     "startup": "enabled",
-                    "environment": {},
                 }
             },
         }
         # Get the kubeflow-volumes container from the model
         container = self.harness.model.unit.get_container("kubeflow-volumes")
-        # Emit the PebbleReadyEvent carrying the httpbin container
+        # Emit the PebbleReadyEvent carrying the kubeflow-volumes container
         self.harness.charm.on.kubeflow_volumes_pebble_ready.emit(container)
         # Get the plan now we've run PebbleReady
         updated_plan = self.harness.get_container_pebble_plan(
@@ -48,3 +50,13 @@ class TestCharm(unittest.TestCase):
         self.assertEqual(expected_plan, updated_plan)
         # Ensure we set an ActiveStatus with no message
         self.assertEqual(self.harness.model.unit.status, ActiveStatus())
+
+    def test_ingress_relation(self):
+        rid = self.harness.add_relation("ingress", "ingress")
+        rel = self.harness.model.get_relation("ingress", rid)
+        self.harness.add_relation_unit(rid, "ingress/0")
+        assert rel.data[self.harness.charm.app] == {
+            "service-hostname": "kubeflow-volumes.juju",
+            "service-name": "kubeflow-volumes",
+            "service-port": 8080,
+        }
