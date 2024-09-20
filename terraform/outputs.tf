b268128905a378289732b2cd1f6d878c513fbaa1
Manos Vlassis <57320708+mvlassis@users.noreply.github.com>
2024-09-20 15:02:56 +0300

    Feat: Add Terraform module (#158)
    * Add terraform modules
    
    * Update outputs
    
    * Update resource name to snake case
    
    * Update resource name
    
    * Update resource name
    
    * Fix formatting
    
    * Fix app name
    
    * Untabify
    
    * Update README.md
    
    * Update README.md

---
+++ terraform/outputs.tf
@@ -0,0 +1,16 @@
+output "app_name" {
+  value = juju_application.kubeflow_volumes.name
+}
+
+output "provides" {
+  value = {
+  }
+}
+
+output "requires" {
+  value = {
+    ingress         = "ingress"
+    dashboard_links = "dashboard-links"
+    logging         = "logging"
+  }
+}
